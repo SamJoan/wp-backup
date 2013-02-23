7d08a603add88e6f7e14c24315bbd0023a0e5250
Pedro Worcel <pedro@worcel.com>
2013-02-24 11:42:34 +1300

    improved logging.

--- wp-backup.sh
+++ wp-backup.sh
@@ -9,15 +9,21 @@ set -e
 logFile=log.txt
 sess='--check-status --session=wp-bak'
 
+# Log all echo.
+function echolog {
+	echo $@
+	echo $@ >> $logFile
+}
+
 # login
 if http $sess -f POST "https://$u.wordpress.com/wp-login.php" log=$u pwd=$p testcookie=1 &>> $logFile; then
 	# Status code 200 means wp didn't like the username and password.
-	echo Wordpress did not like the given username and password. Please check the credentials.sh file.
+	echolog Wordpress did not like the given username and password. Please check the credentials.sh file.
 	exit 1
 else 
 	case $? in
-		3) echo Logged in successfully, will now download file. ;;
-		*) echo Unexpected error! 4xx or 5xx? logs are at $logFile.
+		3) echolog Logged in successfully, will now download file. ;;
+		*) echolog Unexpected error! 4xx or 5xx? logs are at $logFile.
 			exit 1;;
 	esac
 fi
@@ -26,10 +32,11 @@ fi
 date=`date +%Y-%m-%d.%H:%M:%S`
 file=backups/$u.$date.xml
 if http $sess https://$u.wordpress.com/wp-admin/export.php?download=true > $file 2>> $logFile; then
-	echo File \'$file\' created.
+	filesize=`du -h $file`
+	echolog File created. \($filesize\).
 else 
 	case $? in
-		*) echo Unexpected error ocurred! Please check $logFile
+		*) echolog Unexpected error ocurred! Please check $logFile
 			exit 1;;
 	esac
 fi
